6d8829e26d6048726951f55711cf3d617747302e
leastyeast <Katherine.Kisiloski@edu.sait.ca>
2023-10-19 16:09:21 -0600

    step 2+3

--- temp.py
+++ temp.py
@@ -1,3 +1,17 @@
-# calculation of fahrenheit to celsius
-fah = 25 * 9/5 + 32 
-print(f"25 is {fah} in Fahrenheit")
+celsius = int(input("Please input a degree in celsius: \n>>>> "))
+fahrenheit = int(input("Please input a degree in fahrenheit: \n>>>> "))
+
+def celsius_to_fahrenheit(celsius):
+    #program to convert celsius to fahrenheit
+    fah = celsius * 9/5 + 32
+    return fah
+
+def fahrenheit_to_celsius(fahrenheit):
+    cel = (fahrenheit - 32) * 5/9
+    return cel
+
+fah = celsius_to_fahrenheit(celsius)
+cel = fahrenheit_to_celsius(fahrenheit)
+
+print(f"{celsius} is {fah} in Fahrenheit.")
+print(f"{fahrenheit} is {cel} in Celsius.")
